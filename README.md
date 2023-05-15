87ad8897db81bffb0e0af1fac10f2ee9e153f123
Hyeji Kang <127934891+hyejikng@users.noreply.github.com>
2023-05-15 14:55:24 -0700

    added Routers

---
+++ README.md
@@ -0,0 +1,16 @@
+//그룹1(global router)
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+//그룹2(users router)
+/users/edit -> Edit User
+/users/delete -> Delete User
+
+//그룹3(videos router)
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete a Comment of a Video
--- src/server.js
+++ src/server.js
@@ -4,25 +4,39 @@ import morgan from "morgan";
 const PORT = 4000;
 
 const app = express();
-const logger = morgan('dev');
+const logger = morgan("dev");
 app.use(logger);
 
-// const globalRouter 
 
-const gossipMiddleware = (req, res, next) => {
-    console.log("hi I am middleware")
-    next();
-}
+// Global
+const globalRouter = express.Router();
 
-const handleHome = (req, res) => {
-    return res.end();
-}
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+// Users
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+// Videos
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 
-app.get("/", gossipMiddleware, handleHome);
 
-
-
-const handleListening =() => console.log(`Server listening on port ${PORT} 🚀🪐💫`);
+const handleListening =() => 
+    console.log(`Server listening on port ${PORT} 🚀🪐💫`);
 
 app.listen(4000, handleListening);
