7f336c4cc2c59cf843cb615868ff5c4fd62e04de
Hyeji Kang <127934891+hyejikng@users.noreply.github.com>
2023-05-17 17:28:51 -0700

    created controllers

---
+++ src/controllers/userController.js
@@ -0,0 +1,4 @@
+export const join = (req, res) => res.send("Join");
+
+export const edit = (req, res) => res.send("Edit Users");
+export const deleteUser = (req, res) => res.send("Delete Users");
---
+++ src/controllers/videoController.js
@@ -0,0 +1,6 @@
+// 처음 유툽 들어가면 뜨는 페이지에 해당.
+export const trending = (req, res) => res.send("Home Page Videos");
+export const watch = (req, res) => send("Watch Videos");
+export const edit = (req, res) => send("Edit Videos");
+
+  
